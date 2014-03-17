6613d7da06122d1a46b83a990300368c37deb20a
John Miller <c15john.miller@usafa.edu>
2014-03-17 07:56:43 -0600

    created the file utilizing generics, I will test it once I figure out what clock speed we are operating at

--- clk_to_baud.vhd
+++ clk_to_baud.vhd
@@ -22,7 +22,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx primitives in this code.
@@ -30,6 +30,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity clk_to_baud is
+	 generic (N : integer);
     Port ( clk : in  STD_LOGIC;
            reset : in  STD_LOGIC;
            baud_16x_en : out  STD_LOGIC);
@@ -37,8 +38,24 @@ end clk_to_baud;
 
 architecture Behavioral of clk_to_baud is
 
+signal baud_count : integer range 0 to N := 0;
+signal en_16_x_baud : std_logic := '0';
+
 begin
 
+
+	baud_rate: process(clk)
+		begin
+			if rising_edge(clk) then
+				if baud_count = N then
+					baud_count <= 0;
+					en_16_x_baud <= '1';
+				else
+					baud_count <= baud_count + 1;
+					en_16_x_baud <= '0';
+				end if;
+			end if;
+	end process baud_rate;
 
 end Behavioral;
 
